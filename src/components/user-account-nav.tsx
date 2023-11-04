3d7d2377c880e5a96c60ae5e1b0a433ee1c6b86c
mattiapomelli <mattiapomelli@gmail.com>
2023-11-04 01:43:41 +0000

    Fix menu

--- src/components/user-account-nav.tsx
+++ src/components/user-account-nav.tsx
@@ -25,23 +25,14 @@ export function UserAccountNav({ user }: UserAccountNavProps) {
         <AddressAvatar address={user.address as `0x${string}`} className="h-8 w-8" />
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <div className="flex items-center justify-start gap-2 p-2">
-          <div className="flex flex-col space-y-1 leading-none">
-            {user.name && <p className="font-medium">{user.name}</p>}
-            {user.email && (
-              <p className="w-[200px] truncate text-sm text-muted-foreground">{user.email}</p>
-            )}
-          </div>
-        </div>
-        <DropdownMenuSeparator />
         <DropdownMenuItem asChild>
-          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/dashboard">Campaigns</Link>
         </DropdownMenuItem>
         <DropdownMenuItem asChild>
-          <Link href="/dashboard/billing">Billing</Link>
+          <Link href="/dashboard/contacts">Contacts</Link>
         </DropdownMenuItem>
         <DropdownMenuItem asChild>
-          <Link href="/dashboard/settings">Settings</Link>
+          <Link href="/dashboard/lists">Lists</Link>
         </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem
