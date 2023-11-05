07db9dae8b888e0a63f946bca1c14844c61d2dbf
mattiapomelli <mattiapomelli@gmail.com>
2023-11-05 10:45:08 +0000

    Link to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 import { getCurrentUser } from "@/lib/session";
 
 export default async function Home() {
   const user = await getCurrentUser();
 
-  return <>{user ? <div>{user.address}</div> : <div>Not signed in!</div>}</>;
+  return (
+    <>
+      {user ? (
+        <div className="flex justify-center py-8">
+          <Link href={"/dashboard"}>
+            <Button>Go to dashboard</Button>
+          </Link>
+        </div>
+      ) : (
+        <div className="flex justify-center py-8">Not signed in!</div>
+      )}
+    </>
+  );
 }
