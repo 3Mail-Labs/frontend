885da337239027386fe04f769a95424c11c97e4a
mattiapomelli <mattiapomelli@gmail.com>
2023-11-05 10:37:48 +0000

    FIx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import { getCurrentUser } from "@/lib/session";
 export default async function Home() {
   const user = await getCurrentUser();
 
-  return <>{user ? <div>{user.address}</div> : <div>Not signed in</div>}</>;
+  return <>{user ? <div>{user.address}</div> : <div>Not signed in!</div>}</>;
 }
